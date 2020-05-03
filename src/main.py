d9fc8d1f94adc0463374cb4f731c9e2cc0c34981
Dan Tablac <dantablac@Dans-MacBook-Pro.local>
2020-05-03 09:48:11 -0700

    Just added a file

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# main.py
+# Dan Tablac: [ID]
+# Anthony Esmeralda: [ID]
+
+if __name__ == '__main__':
+    while True:
+        x = input('Please enter a query ("quit" to quit): ')
+        print(x)
+        if (x == 'quit'):
+            break
+        
