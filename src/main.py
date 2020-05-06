0ef7192a13293299bce6dbf5e31103372eef4a60
Dan Tablac <dantablac@Dans-MBP.lan1>
2020-05-06 12:19:15 -0700

    Inverted index displays for the first 3 docs of the first domain. Swapped to (doc_id,url) from (url,doc_id) in doc_ids dict

--- src/main.py
+++ src/main.py
@@ -9,65 +9,96 @@
 
 import os
 import json
+import re
+from bs4 import BeautifulSoup
 
 # ---------- Global Variables ---------- #
 
 doc_id_counter = 1    # Unique doc_id counter
 
-index = dict()      # Store word -> posting pairings.
+inverted_index = dict()      # Store word(str) -> posting(list) pairings.
 
-doc_ids = dict()    # Store doc_name -> id pairings
+doc_ids = dict()    # Store doc_id -> doc_name pairings
 
 # ---------- Index Implementation ---------- #
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+# store postings in python STL list
 
 class Posting:
     def __init__(self, id, score):
         self.id = id                # document id that token was found (from doc_ids)
         self.score = score          # "tf_ifd score"
-        self.next = None
+
+    def __repr__(self):
+        return str('(doc_id: {}, score: {})'.format(self.id, self.score))
 
 # ------------------------------------------ #
 
-def assign_doc_id(document):
+def _assign_doc_id(document):
     ''' Assign document id to a document. '''
     global doc_ids
     global doc_id_counter
 
-    doc_ids[document] = doc_id_counter
+    doc_ids[doc_id_counter] = document
+    returning_doc_id = doc_id_counter    # temp store of original doc_id before incrementing
     doc_id_counter += 1
+    return returning_doc_id
 
-def access_json_files():
-    ''' Access each domain folder and their respected json files. Prints the url in the json obj '''
+def _add_posting(token, id):
+    ''' Adds/updates a posting, of the token's occurence in a document (id), in the inverted index '''
+    global inverted_index
+    token2 = token.lower()
+    posting_updated = False
+    try:
+        for posting in inverted_index[token2]:
+            if posting.id == id:
+                posting.score += 1
+                posting_updated = True
+                break
+        if posting_updated == False:
+            inverted_index[token2].append(Posting(id, 1))
+    except KeyError:
+        inverted_index[token2] = []
+        inverted_index[token2].append(Posting(id, 1))
+
+def access_json_files(root):
+    ''' Access each domain folder and their respected json files. '''
     # directory concept: https://realpython.com/working-with-files-in-python/#listing-all-files-in-a-directory
     # json concept: https://www.geeksforgeeks.org/read-json-file-using-python/
+    # re and BeautifulSoup documentations referenced
 
-    root = '../DEV'                 # 'DEV' directory contains domains (extract developer.zip first)
     corpus = os.listdir(root)    # list containing each domain in 'DEV'
     
-    for domain in corpus:
+    # --- Check each domain (folder) --- #
+    for domain in corpus[:1]:
         if os.path.isdir(os.path.join(root, domain)):  # Only get folders. Ignores .DS_Store in mac
             print('----------{}----------'.format(domain))  # just for showing which urls belong to what
             
-            sub_dir = '{}/{}'.format(root,domain)           # Domain within root
+            sub_dir = '{}/{}'.format(root,domain)           # Path to domain within root
             pages = os.listdir(sub_dir)                     # list json files, or 'pages', in domain
-            for page in pages:
-                assign_doc_id(page)                         # give json file a unique doc_id
-                json_file_location = sub_dir + '/{}'.format(page) # location of json file
+            for page in pages[:3]:
+                id = _assign_doc_id(page)                         # give json file a unique doc_id
+                json_file_location = sub_dir + '/{}'.format(page) # Path to page
 
                 file = open(json_file_location, 'r')              # open JSON file
                 data = json.load(file)                            # JSON object becomes dict
                 file.close()
 
                 # --- Do Stuff with the data --- #
-                #print(data['url'])
+                soup = BeautifulSoup(data['content'], 'html.parser')
+                tokens = re.findall('[a-zA-Z0-9]+',soup.get_text())
+                for token in tokens:
+                    _add_posting(token, id)
 
 # ------------------------------------------ #
 
+# For now, just prints the the tokens and postings in the first domain for 3 documents (indices above)
+
 if __name__ == '__main__':
-    access_json_files()
+    access_json_files('../DEV') # 'DEV' directory contains domains (extract developer.zip first)
+    for word in inverted_index:
+        postings = inverted_index[word]
+        if len(postings) > 1:
+            print('{} : {}'.format(word, postings))
     print('Number of documents: {}'.format(doc_id_counter-1))
     
