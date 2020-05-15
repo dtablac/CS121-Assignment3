3eb3b479f51c449c2e03d703ec5e411aef42cde4
Dan Tablac <dantablac@Dans-MacBook-Pro.local>
2020-05-15 13:16:15 -0700

    Minor fixes, comments

--- src/search_query.py
+++ src/search_query.py
@@ -28,14 +28,6 @@ def create_threads(query_list):
         threads.append(threading.Thread(target=get_postings,args=(query,)))
     return threads
 
-def _list_doc_ids(postings: list):
-    ''' Get doc_ids [x,x2,...] from posting list [[x,y],[x2,y2],...] '''
-    doc_ids = []
-    for posting in postings:
-        doc_ids.append(posting[0])
-    return doc_ids
-
-
 def show_urls():
     ''' Prints the top 5 urls from the search (AND only) '''
     global values, doc_ids
@@ -66,6 +58,15 @@ def show_urls():
         # --- If no URLs from retrieval and/or intersection --- #
         return None
 
+### -------------------------- Helper Functions -------------------------- ###
+
+def _list_doc_ids(postings: list):
+    ''' Get doc_ids [x,x2,...] from posting list [[x,y],[x2,y2],...] '''
+    doc_ids = []
+    for posting in postings:
+        doc_ids.append(posting[0])
+    return doc_ids
+
 def _render_response(result: list):
     ''' Formats top 5 urls for page render '''
 
@@ -73,7 +74,7 @@ def _render_response(result: list):
     if result == None:
         html_result = "<h3>No search results found</h3>"
     else:
-        html_result = '<div><ol class="results">'
+        html_result = '<div class="fade"><ol class="results">'
         for i in range(len(result)):
             html_result += '<li><a href={}>{}</a></li>'.format(result[i], result[i])
         html_result += '</ol></div>'
@@ -83,6 +84,9 @@ def _render_response(result: list):
     result_file.write(html_result)
     result_file.close()
 
+### -------------------------- API Endpoints -------------------------- ###
+
+# --- Home page loads on first visit or home url entered in browser --- #
 @app.route('/')
 def main():
     result_file = open('templates/results.html','w')
@@ -90,36 +94,49 @@ def main():
     result_file.close()
     return render_template('index.html')
 
+# --- Flask routes POST requests made from index.html --- #
 @app.route('/', methods=['POST'])
 def run_search():
-    #search_query = input("Search for: ").lower()
-    user_query = request.form['search']
+    user_query = request.form['search']    # Contents of search bar
+
+    # --- Do stuff with search query --- #
     search_query = user_query.lower()
     search_query = search_query.split()
     search_query = [ps.stem(item) for item in search_query]
     values.clear()
+
     start = time.time()
 
+    # --- Threads fill 'values' dict with urls --- #
     threads = create_threads(search_query)
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
-        
+
+    # --- Update 'results.html' template with urls found --- #
     _render_response(show_urls())
+
     runtime = (time.time() - start) * 1000
+
     timestamp = 'Retrieved in {} ms.'.format(runtime)
 
+    # --- Render results (check index.html), query, and time --- #
     return render_template('index.html', query=user_query, time=timestamp)
-    # values will hold our data
+
+### ---------- Main ---------- ###
 
 if __name__ == '__main__':
+    # --- Load index of tokens mapped to the file's line. --- # 
+    #     This prevents loading entire index to memory.       #
     f = open('index_posting_location.txt','r')                        
-    ip_loc = json.load(f)        # load the index of tokens mapped to the file's line; so we dont load the entire index into memory
+    ip_loc = json.load(f)
     values = {}
     f.close()
-    ps = PorterStemmer()
-    doc_ids_file = open('doc-ids.txt','r')
+
+    ps = PorterStemmer()    # Stems tokens in user query
+
+    doc_ids_file = open('doc-ids.txt','r')    # Load
     doc_ids = json.load(doc_ids_file)
     doc_ids_file.close()
 
